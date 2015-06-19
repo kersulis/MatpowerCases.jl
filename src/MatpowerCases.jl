51988081f7fa3d44c7f5970fe0c6781cf63ccabf
Jonas Kersulis <kersulis@umich.edu>
2015-06-19 16:34:40 -0400

    added casenames() function to retrieve a list of valid case names

--- src/MatpowerCases.jl
+++ src/MatpowerCases.jl
@@ -16,15 +16,49 @@ function loadcase(caseName::ASCIIString)
     case30pwl
     case30
     case30Q
-    case39
+    case391
     case4gs
     case57
     case6ww
     case9
     case9
     case9Q
+    case79
+    case96
+    Return this list of cases by running casenames().
     """
     matread("$(dirname(@__FILE__))/$(caseName).mat")["networkData"]
+end
+
+function casenames()
+    """ Return an array containing all valid
+    MatpowerCases case names.
+    """
+    return [
+    "case118",
+    "case14",
+    "case2383wp",
+    "case24_ieee_rts",
+    "case2736sp",
+    "case2737sop",
+    "case2746wop",
+    "case2746wp",
+    "case300",
+    "case3012wp",
+    "case30",
+    "case30pwl",
+    "case30Q",
+    "case3120sp",
+    "case3375wp",
+    "case39",
+    "case4gs",
+    "case57",
+    "case6ww",
+    "case9",
+    "case9Q",
+    "case79",
+    "case96"
+    ]
 end
 
 end
