f727b8ef19a4f4eb401a650384ddff41737d7a49
Jonas Kersulis <kersulis@umich.edu>
2015-07-15 12:29:19 -0500

    1. Move all `.mat` files to their own `data` directory, out of `src`.
    2. Include four new networks released with MATPOWER 5.1.
    
    3. Add `docstring` key to returned `mpc` containing detailed
    network information provided by MATPOWER in the original `.m` file.

--- src/MatpowerCases.jl
+++ src/MatpowerCases.jl
@@ -3,62 +3,26 @@ using MAT
 
 export loadcase,casenames
 
+"""
+Return a Dict containing power system data
+in MATPOWER's format.
+Return this list of cases by running casenames().
+"""
 function loadcase(caseName::ASCIIString)
-    """
-    Return a Dict containing power system data 
-    in MATPOWER's format. Valid cases:
-    case118
-    case118
-    case14
-    case14
-    case24_ieee_rts
-    case300
-    case30pwl
-    case30
-    case30Q
-    case391
-    case4gs
-    case57
-    case6ww
-    case9
-    case9
-    case9Q
-    case79
-    case96
-    Return this list of cases by running casenames().
-    """
-    matread("$(dirname(@__FILE__))/$(caseName).mat")["networkData"]
+    s = joinpath(Pkg.dir(),"MatpowerCases","data")
+    mpc = matread(joinpath(s,"$(caseName).mat"))["mpc"]
+    ds = mpc["docstring"]
+    println(split(ds,"\n")[1])
+    return mpc
 end
 
 function casenames()
     """ Return an array containing all valid
     MatpowerCases case names.
     """
-    return [
-    "case118",
-    "case14",
-    "case2383wp",
-    "case24_ieee_rts",
-    "case2736sp",
-    "case2737sop",
-    "case2746wop",
-    "case2746wp",
-    "case300",
-    "case3012wp",
-    "case30",
-    "case30pwl",
-    "case30Q",
-    "case3120sp",
-    "case3375wp",
-    "case39",
-    "case4gs",
-    "case57",
-    "case6ww",
-    "case9",
-    "case9Q",
-    "case79",
-    "case96"
-    ]
+    s = joinpath(Pkg.dir(),"MatpowerCases","data")
+    names = readdir(s)
+    [splitext(name)[1] for name in names]
 end
 
 end
